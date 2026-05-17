9cfdc7246bba8e400c1fded4b5d43afe6e25f53d
eva <evajoyanderson@gmail.com>
2026-05-18 11:57:43 +1200

    added to route domain object

--- transport/src/main/java/com/publictransport/transport/route/domain/Route.java
+++ transport/src/main/java/com/publictransport/transport/route/domain/Route.java
@@ -1,5 +1,26 @@
 package com.publictransport.transport.route.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+// Using routes.txt to build domain object
+
+@Entity
 public class Route {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String routeId;
+    private String agencyId;    
+    private String routeShortName;
+    private String routeLongName;
+    private String routeDescription;
+    private Integer routeType;
+    private String routeUrl;
+    private String routeColor;
+    private String routeTextColor; 
+    private Integer routeSortOrder;
+
+    public Route() {}
 }
