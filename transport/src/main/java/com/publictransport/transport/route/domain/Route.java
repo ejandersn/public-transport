3ee80336ce2366d42b3b0cde3ca54bb1a7838e46
eva <evajoyanderson@gmail.com>
2026-05-18 14:33:51 +1200

    getters and setters for route domain obj

--- transport/src/main/java/com/publictransport/transport/route/domain/Route.java
+++ transport/src/main/java/com/publictransport/transport/route/domain/Route.java
@@ -23,4 +23,86 @@ public class Route {
     private Integer routeSortOrder;
 
     public Route() {}
+
+    //no get or set for obj id
+
+    public String getRouteID(){
+        return routeId;
+    }
+
+    public String getAgencyId(){
+        return agencyId;
+    }
+
+    public String getRouteShortName(){
+        return routeShortName;
+    }
+
+    public String getRouteLongName(){
+        return routeLongName;
+    }
+
+    public String getRouteDescription(){
+        return routeDescription;
+    }
+
+    public Integer getRouteType(){
+        return routeType;
+    }
+
+    public String getRouteUrl(){
+        return routeUrl;
+    }
+
+    public String getRouteTextColor(){
+        return routeTextColor;
+    }
+
+    public String getColor(){
+        return routeColor;
+    }
+
+    public Integer getRouteSortOrder(){
+        return routeSortOrder;
+    }
+
+    public void setRouteID(String routeID){
+        this.routeId = routeID;
+    }
+
+    public void setAgencyID(String agencyID){
+        this.agencyId = agencyID;
+    }
+
+    public void setRouteShortName(String routeShortName){
+        this.routeShortName = routeShortName;
+    }
+
+    public void setRouteLongName(String routeLongName){
+        this.routeLongName = routeLongName;
+    }
+
+    public void setRouteDescription(String routeDescription){
+        this.routeDescription = routeDescription;
+    }
+
+    public void setRouteType(Integer routeType){
+        this.routeType = routeType;
+    }
+
+    public void setRouteUrl(String routeUrl){
+        this.routeUrl = routeUrl;
+    }
+
+    public void setRouteColor(String routeColor){
+        this.routeColor = routeColor;
+    }
+
+    public void setRouteTextColor(String routeTextColor){
+        this.routeTextColor = routeTextColor;
+    }
+
+    public void setRouteSortOrder(Integer routeSortOrder){
+        this.routeSortOrder = routeSortOrder;
+    }
 }
--- transport/src/main/java/com/publictransport/transport/route/repository/RouteRepository.java
+++ transport/src/main/java/com/publictransport/transport/route/repository/RouteRepository.java
@@ -1,5 +1,12 @@
 package com.publictransport.transport.route.repository;
 
+import com.publictransport.transport.route.domain.Route;
+
 public class RouteRepository {
+
+    public void save(Route route) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
     
 }
